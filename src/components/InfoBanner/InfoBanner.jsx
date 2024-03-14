960b1319534950e968a52bc5280f92c42a58fc1b
whatevaaar <emilianohidalgo314@gmail.com>
2024-03-13 23:43:55 -0600

    Fixed banner formating

--- src/components/InfoBanner/InfoBanner.jsx
+++ src/components/InfoBanner/InfoBanner.jsx
@@ -3,11 +3,20 @@ import SelectedShowContext from "../../controller/SelectedShowContext";
 const InfoBanner = () => {
     const { selectedShow } = useContext(SelectedShowContext);
     const { name, date_begin, date_end, duration } = selectedShow;
+    const getFormattedDate = (date) => date.split(' ')[1].slice(0, -3) + 'hrs ';
+    const getCleanDuration = (duration) => {
+        const splitDuration = duration.split(':');
+        const hour = parseInt(splitDuration[0]) + 'hr';
+        const min = parseInt(splitDuration[1]) + 'mn';
+        return hour + ' ' + min;
+    }
+
     return <section className="info-banner">
         <div>
             <h1>{name}</h1>
             <div className="info-banner-body">
-                <p>{date_begin} a {date_end} {duration}</p>
+                <p>{getFormattedDate(date_begin)} a {getFormattedDate(date_end)}
+                    {getCleanDuration(duration)}</p>
             </div>
         </div>
     </section>
